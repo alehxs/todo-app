45ac40cd46dfe60e5c5a94bd4d2ea8fa01515204
Alex <86182397+alehxs@users.noreply.github.com>
2025-10-05 23:35:44 -0500

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import TasksForm from "./components/tasks-form.svelte";
+    import TasksList from "./components/tasks-list.svelte";
+    import type { Task } from "./types";
+
+    let message = "Tasks App";
+    let tasks = $state<Task[]>([]);
+    let totalDone = $derived(
+        tasks.reduce((total, task) => total + Number(task.done), 0),
+    );
+
+    function addTask(newTask: string) {
+        tasks.push({
+            id: crypto.randomUUID(),
+            title: newTask,
+            done: false,
+        });
+    }
+
+    function toggleDone(task: Task) {
+        task.done = !task.done!;
+    }
+
+    function removeTask(index: number) {
+        tasks.splice(index, 1);
+    }
+</script>
+
+<main>
+    <h1>{message}</h1>
+    <TasksForm {addTask} />
+    <p>
+        {#if tasks.length}
+            {totalDone} / {tasks.length} tasks completed
+        {:else}
+            Add a task to get started.
+        {/if}
+    </p>
+    <TasksList {tasks} {toggleDone} {removeTask} />
+</main>
+
+<style>
+    main {
+        margin: 1rem auto;
+        max-width: 800px;
+    }
+</style>
